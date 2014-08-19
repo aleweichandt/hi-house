4016adcb2e29a49a04514a3fd031ceb9d41f6ea8
aleweichandt@gmail.com <aleweichandt@gmail.com@b2a8dadd-85fd-8445-52ba-247269a4d35f>
2014-08-19 16:12:34 +0000

    +agrego interfaz con db

--- trunk/server/src/server/model/UserSession.java
+++ trunk/server/src/server/model/UserSession.java
@@ -1,25 +1,38 @@
 package server.model;
 
+import java.util.Date;
+
 public class UserSession {
 	
-	//private User mUser
+	private User mUser;
 	private String mToken;
-	//private mExpirationTime
+	private Date mExpirationTime;
 	
 	
 	private static int counter = 0; //For testing purpose
 	
-	public UserSession(String userid) {
-		//generar usuario
+	public UserSession(User user) {
+		mUser = user;
+		renewSessionTime();
 		mToken = String.valueOf(counter++);//for testing purpose
 	}
 	
 	public boolean isValid() {
-		//TODO check expiration time
-		return true;
+		Date now = new Date();
+		boolean ret = now.before(mExpirationTime);
+		if(ret) renewSessionTime(); //renovar si es valido
+		return ret;
 	}
 	
 	public String getToken() {
 		return mToken;
+	}
+	
+	private void renewSessionTime() {
+		mExpirationTime = new Date(System.currentTimeMillis() + (15*60*1000));//15 mins from now
+	}
+	
+	public User getUser() {
+		return mUser;
 	}
 }
