6c9c08ae137da5dba18d7a9de5053bd2942ae0e3
aleweichandt <aleweichandt@gmail.com>
2014-10-09 01:08:19 +0000

    +agrego configuracion del server desde la actividad de login

--- client/src/com/web/ones/hihouse/Command.java
+++ client/src/com/web/ones/hihouse/Command.java
@@ -2,12 +2,15 @@ package com.web.ones.hihouse;
 
 public class Command {
 
-	public final static String serverURL = "http://192.168.1.110:8080/HiHouse/";
-	//public final static String serverURL = "http://190.192.169.86:8080/HiHouse/";
+	public static String serverURL = "http://192.168.1.101:8080/HiHouse/";
 	private boolean method;
 	private String requestURL;
 	private String bodyParams;
 	private int type;
+	
+	public static void setCommandServerBase(String url) {
+		serverURL = url + "/HiHouse/";
+	}
 
 	public Command(int type, boolean method, String requestURL, String params){
 		this.type = type;
