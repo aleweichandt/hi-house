bac2fe3f321eaecceac146c340f7b67d35a2242d
aleweichandt <aleweichandt@gmail.com>
2014-10-16 18:17:30 +0000

    !agregando seguridad -login manda password hasheada al server !dejo la funcion en el form de usuarios para usarla cuando se crea un nuevo usuario o se edite alguno. -cambios en el setting de la url del server para poder ver los cambios que se hacen desde la app

--- client/src/com/web/ones/hihouse/UserInfoFragment.java
+++ client/src/com/web/ones/hihouse/UserInfoFragment.java
@@ -208,4 +208,18 @@ OnMultiChoiceDialogListener{
 		
 	}
 
+//password must be hashed before sent using MD5
+	protected String getMD5(String text) {
+		try {
+			java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+			byte[] array = md.digest(text.getBytes());
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < array.length; ++i) {
+				sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+			}
+			return sb.toString();
+		} catch (java.security.NoSuchAlgorithmException e) {
+		}
+		return null;
+	}
 }
