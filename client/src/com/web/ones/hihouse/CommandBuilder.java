fe110c63e8be05067f15cfd2e3b6fa312d322de8
ivanpalma89 <ivanpalma89@gmail.com>
2014-10-17 06:34:20 +0000

    Comandos de Voz: * Dispositivos: Puertas(abrir/cerrar) - Luz/Aires(prender-encender/apagar) - Sensores(activar/desactivar) * Temperatura: Subir/Bajar temperatura <num> - temperatura <num> - apagar temperatura * Alarma: activar/desactivar alarma/seguridad

--- client/src/com/web/ones/hihouse/CommandBuilder.java
+++ client/src/com/web/ones/hihouse/CommandBuilder.java
@@ -1,9 +1,12 @@
 package com.web.ones.hihouse;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 
+import android.widget.Toast;
+
 public class CommandBuilder {
-	private ArrayList<String> commands;
+	private ArrayList<String> irrelevantWords;
 	private HiHouse hiHouse;
 	
 	
@@ -11,54 +14,108 @@ public class CommandBuilder {
 		
 		this.hiHouse = activity; 
 		
-		commands = new ArrayList<String>();
-		commands.add("abrir");
-		commands.add("cerrar");
-		commands.add("prender");
-		commands.add("encender");
-		commands.add("apagar");
-		commands.add("activar");
-		commands.add("desactivar");
+		irrelevantWords = new ArrayList<String>();
+		irrelevantWords.add("a");
+		irrelevantWords.add("la");
 	}
 
 	public Command generateCommand(ArrayList<String> matches) {
-		String result="";
 		int i;
-		String command="", device="", com, dev;
-		boolean command_set=false, device_set=false;
+		String command;
+		String delims = "[ ]+";
 		
-		//primero obtengo el comando
+		//checkeo el comando
 		for (String s : matches){
-			if((i = s.indexOf(' '))<=0) return null;
-			com = s.substring(0, i);
-			
-			if("prender".equals(com) || "encender".equals(com) || "abrir".equals(com)){
-				command = com;
-				int deviceId = getDeviceId(matches);
-				if(deviceId!=-1)
-					return new Command(Request.SET_DEVICE_STATE,false, "devices/"+deviceId+"/state?enabled=true&token="+hiHouse.getUser().getToken(), "");
-				return null;
+			if(s.indexOf(' ')<=0) return null; //si no hay espacio es una sola palabra
+			String[] tokens = s.split(delims);
+			i = 0;
+			try{
+				command = tokens[i++];
+				
+				String voice_desc = s.substring(s.indexOf(' ')+1);
+				Device device = hiHouse.getUser().getDeviceByVoiceDesc(voice_desc);
+				if(device==null){ //probamos nuevamente removiendo acentos
+					voice_desc = Normalizer.normalize(voice_desc, Normalizer.Form.NFD);
+					voice_desc = voice_desc.replaceAll("[^\\p{ASCII}]", "");
+					device = hiHouse.getUser().getDeviceByVoiceDesc(voice_desc);
+				}
+				if(device!=null){
+					switch(device.getType()){
+					case Device.DEVICE_TYPE_AC_DOOR: // abrir/cerrar
+						if(command.equals("abrir")) return setDeviceStateCommand(device, true);
+						else if(command.equals("cerrar")) return setDeviceStateCommand(device, false);
+						break;
+					case Device.DEVICE_TYPE_AC_LIGHT: // prender-encender/apagar
+					case Device.DEVICE_TYPE_AC_TERMAL:
+						if(command.equals("prender")||command.equals("encender")) return setDeviceStateCommand(device, true);
+						else if(command.equals("apagar")) return setDeviceStateCommand(device, false);
+						break;
+					case Device.DEVICE_TYPE_SN_LIGHT: // activar/desactivar
+					case Device.DEVICE_TYPE_SN_TERMAL:
+						if(command.equals("activar")) return setDeviceStateCommand(device, true);
+						else if(command.equals("desactivar")) return setDeviceStateCommand(device, false);
+					}
+				}
+				else if(command.equals("subir") || command.equals("bajar")) return tempCommand(tokens, i);
+				else if(command.equals("temperatura")) return tempCommand(tokens, --i);
+				else if(command.equals("apagar")) return tempOffCommand(tokens, i);
+				else if(command.equals("activar")) return alarmCommand(tokens, i, true);
+				else if(command.equals("desactivar")) return alarmCommand(tokens, i, false);
+				else{
+					Toast.makeText(hiHouse, "Comando no reconocido", Toast.LENGTH_LONG).show();
+					return null;
+				}
 			}
-			if("apagar".equals(com) || "cerrar".equals(com)){
-				command = com;
-				int deviceId = getDeviceId(matches);
-				if(deviceId!=-1)
-					return new Command(Request.SET_DEVICE_STATE,false, "devices/"+deviceId+"/state?enabled=false&token="+hiHouse.getUser().getToken(), "");
-				return null;
-			}
+			catch(ArrayIndexOutOfBoundsException e){Toast.makeText(hiHouse, "Comando no reconocido", Toast.LENGTH_LONG).show();return null;}
 		}
-		
+		Toast.makeText(hiHouse, "Comando no reconocido", Toast.LENGTH_LONG).show();
 		return null;
 	}
 
-	private int getDeviceId(ArrayList<String> matches) {		
-		int i;
-		for (String s : matches){
-			if((i = s.indexOf(' '))<=0) return -1;
-			int deviceId = hiHouse.getUser().getDeviceByVoiceDesc(s.substring(i+1));
-			if(deviceId!=-1) return deviceId;
-		}
-		return -1;
+	private Command setDeviceStateCommand(Device d, boolean b) {
+		return new Command(Request.SET_DEVICE_STATE,false, "devices/"+d.getId()+"/state?enabled="+b+"&token="+hiHouse.getUser().getToken(), "");
 	}
 
+	private Command alarmCommand(String[] tokens, int i, boolean b) {
+		i = skipIrrelevantWord(tokens[i], i);
+		String command = tokens[i];
+		if(command.equals("alarma") || command.equals("seguridad")){
+			return new Command(Request.ALARM_STATE, false, "security/state?token="+hiHouse.getUser().getToken(), ""+b);
+		}
+		Toast.makeText(hiHouse, "Comando no reconocido", Toast.LENGTH_LONG).show();
+		return null;
+	}
+
+	private Command tempOffCommand(String[] tokens, int i) {
+		i = skipIrrelevantWord(tokens[i], i);
+		String command = tokens[i++];
+		if(command.equals("temperatura")){
+			return new Command(Request.VOICE_SET_DESIRED_TEMP, false, "temperature?token="+hiHouse.getUser().getToken(), "-1");
+		}
+		Toast.makeText(hiHouse, "Comando no reconocido", Toast.LENGTH_LONG).show();
+		return null;
+	}
+	
+	private Command tempCommand(String[] tokens, int i) {
+		i = skipIrrelevantWord(tokens[i], i);
+		String command = tokens[i++];
+		if(command.equals("temperatura")){
+			//obtenemos los grados
+			i = skipIrrelevantWord(tokens[i], i);
+			int temp;
+			try{temp = Integer.parseInt(tokens[i++]);}catch(NumberFormatException e){Toast.makeText(hiHouse, "Comando no reconocido", Toast.LENGTH_LONG).show();return null;}
+			if(temp<15 || temp>30){
+				Toast.makeText(hiHouse, "La temperatura debe estar entre 15 y 30°", Toast.LENGTH_SHORT).show();
+				return null;
+			}
+			return new Command(Request.VOICE_SET_DESIRED_TEMP, false, "temperature?token="+hiHouse.getUser().getToken(), ""+temp);
+		}
+		Toast.makeText(hiHouse, "Comando no reconocido", Toast.LENGTH_LONG).show();
+		return null;
+	}
+
+	private int skipIrrelevantWord(String token, int i) {
+		if(irrelevantWords.contains(token)) return i+1;
+		return i;
+	}
 }
