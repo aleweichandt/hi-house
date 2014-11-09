9cacbdba0e6021ce709bd7507ee7df6277dcaab8
aleweichandt <aleweichandt@gmail.com>
2014-11-09 19:10:23 +0000

    +agrego archivo de configuracion *angulo del servo a 75 grados

--- server/src/server/model/BackgroundServlet.java
+++ server/src/server/model/BackgroundServlet.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -24,12 +27,59 @@ public class BackgroundServlet extends GenericServlet{
 	
 	@Override
 	public void init() throws ServletException {
+		readConfig();
 		TimerTask srTimer = new ServicesTask();
 		TimerTask arTimer = new ArduinoTask();
 		Timer timer = new Timer();
 		timer.schedule(srTimer, 1000, 1000);
 		timer = new Timer();
 		timer.schedule(arTimer, 1000, 1000);
+	}
+	
+	private void readConfig() {
+		Context initCtx;
+		try {
+			initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+		//Arduino serial port
+			String ardPort = (String) envCtx.lookup("ARDUINO_PORT_NAME");
+			if(ardPort != null) C.Config.ARDUINO_PORT_NAME = ardPort;
+			Integer ardRead = (Integer) envCtx.lookup("ARDUINO_PORT_READ");
+			if(ardRead != null) C.Config.ARDUINO_PORT_READ = ardRead;
+		//MySQL Database			
+			String dbLoc = (String) envCtx.lookup("DATABASE_LOCATION");
+			if(dbLoc != null) C.Config.DATABASE_LOCATION = dbLoc;
+			String dbUsr = (String) envCtx.lookup("DATABASE_USER");
+			if(dbUsr != null) C.Config.DATABASE_USER = dbUsr;
+			String dbpwd = (String) envCtx.lookup("DATABASE_PASSWORD");
+			if(dbpwd != null) C.Config.DATABASE_PASSWORD = dbpwd;
+		//Ambient Manager
+			Integer ambUT = (Integer) envCtx.lookup("AMBIENT_UPDATE_TIME");
+			if(ambUT != null) C.Config.AMBIENT_UPDATE_TIME = ambUT;
+			Float ambDeg = (Float) envCtx.lookup("AMBIENT_MAX_DIFF_DEGREES");
+			if(ambDeg != null) C.Config.AMBIENT_MAX_DIFF_DEGREES = ambDeg;
+		//Security Manager
+			Integer secUT = (Integer) envCtx.lookup("SECURITY_UPDATE_TIME");
+			if(secUT != null) C.Config.SECURITY_UPDATE_TIME = secUT;
+			Float secLgL = (Float) envCtx.lookup("SECURITY_SN_LIGHT_LIMIT");
+			if(secLgL != null) C.Config.SECURITY_SN_LIGHT_LIMIT = secLgL;
+			Float secTmL = (Float) envCtx.lookup("SECURITY_SN_TERMAL_LIMIT");
+			if(secTmL != null) C.Config.SECURITY_SN_TERMAL_LIMIT = secTmL;
+			Integer secTO = (Integer) envCtx.lookup("SECURITY_ALERT_TIME_OFFSET");
+			if(secTO != null) C.Config.SECURITY_ALERT_TIME_OFFSET = secTO;
+			String secTtl = (String) envCtx.lookup("SECURITY_ALERT_MSG_TITLE");
+			if(secTtl != null) C.Config.SECURITY_ALERT_MSG_TITLE = secTtl;
+			String secMsg = (String) envCtx.lookup("SECURITY_ALERT_MSG_CONTENT");
+			if(secMsg != null) C.Config.SECURITY_ALERT_MSG_CONTENT = secMsg;
+		//Simulation Manager
+			Integer simMinUT = (Integer) envCtx.lookup("SIMULATION_MIN_UPDATE_TIME");
+			if(simMinUT != null) C.Config.SIMULATION_MIN_UPDATE_TIME = simMinUT;
+			Integer simMaxUT = (Integer) envCtx.lookup("SIMULATION_MAX_UPDATE_TIME");
+			if(simMaxUT != null) C.Config.SIMULATION_MAX_UPDATE_TIME = simMaxUT;
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 	}
 
 	class ServicesTask extends TimerTask {
