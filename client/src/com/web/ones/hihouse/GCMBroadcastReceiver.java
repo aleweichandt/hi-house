211df1f0702e3e01e5685f34649f31d05fd7fd61
ivanpalma89 <ivanpalma89@gmail.com>
2014-11-13 16:36:14 +0000

    Encode y Decode de los mensajes de GCM.

--- client/src/com/web/ones/hihouse/GCMBroadcastReceiver.java
+++ client/src/com/web/ones/hihouse/GCMBroadcastReceiver.java
@@ -1,17 +1,34 @@
 package com.web.ones.hihouse;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.widget.Toast;
 
 public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if(HiHouse.s_activity != null) {
-			//TODO do something
+			String title = "";
+			String content = "";
+			Bundle extras = intent.getExtras();
+			if (!extras.isEmpty()){
+				title = intent.getExtras().getString("title","");
+     	   		try {
+					content = URLDecoder.decode(intent.getExtras().getString("content",""),"UTF-8");
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			Toast.makeText(context, title +" - "+content, Toast.LENGTH_SHORT).show();
 		} else{
 	        ComponentName comp = new ComponentName(context.getPackageName(),
 	                GCMIntentService.class.getName());
